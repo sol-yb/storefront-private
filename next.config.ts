cdf419ab2df307c0f720c096136146a8f600b891
Solomon <solomonyehualashet30@gmail.com>
2026-03-31 16:55:29 +0200

    Fixed `remotePatterns`

--- next.config.ts
+++ next.config.ts
@@ -17,28 +17,14 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
+        protocol: "https",
+        hostname: "**.vendo.dev",
+        pathname: "/rails/active_storage/**",
       },
       {
         protocol: "https",
-        hostname: "**.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.cloudfront.net",
-      },
-      {
-        protocol: "https",
-        hostname: "**.digitaloceanspaces.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.blob.core.windows.net",
+        hostname: "**.spree.sh",
+        pathname: "/rails/active_storage/**",
       },
     ],
   },
